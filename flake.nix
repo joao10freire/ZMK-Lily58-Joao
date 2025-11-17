790b8d66dc2836125efb5e47ec6c9a6dd87b447b
joao10freire <joao.af@outlook.com>
2025-11-17 19:29:59 -0300

    flake.nix correto
    flake.nix correto e completo, exatamente como deve ficar

--- flake.nix
+++ flake.nix
@@ -4,7 +4,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
-    zmk.url = "github:zmkfirmware/zmk";
+
+    zmk = {
+      url = "github:zmkfirmware/zmk";
+      flake = false;   # ← ESSENCIAL
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils, zmk }:
@@ -13,25 +17,27 @@
         pkgs = import nixpkgs { inherit system; };
       in {
         devShells.default = pkgs.mkShell {
-          name = "zmk";
+          name = "zmk-env";
 
           buildInputs = [
             pkgs.cmake
             pkgs.gcc
             pkgs.gnumake
             pkgs.ninja
-            pkgs.gcc-arm-embedded
-            pkgs.dt-toolchain
+            pkgs.git
             pkgs.python3
             pkgs.python3Packages.west
             pkgs.python3Packages.pyelftools
             pkgs.python3Packages.pyyaml
             pkgs.python3Packages.pillow
-            pkgs.git
+            pkgs.gcc-arm-embedded
+            pkgs.dt-toolchain
           ];
 
+          ZMK_PATH = "${zmk}";
+
           shellHook = ''
-            echo "ZMK environment loaded!"
+            echo "ZMK environment loaded with non-flake ZMK source"
           '';
         };
       });
